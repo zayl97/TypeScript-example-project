1d2d12ad6968e45d1a9c52285926e4304b6a36a3
Kamil <kamil.plociennik@edu.wsti.pl>
2022-02-18 13:30:51 +0100

    Prepared TypeScript project to the push

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NewTodo from "./components/NewTodo";
+import Todos from "./components/Todos";
+
+function App() {
+  return (
+    <div>
+      <NewTodo />
+      <Todos />
+    </div>
+  );
+}
+
+export default App;
